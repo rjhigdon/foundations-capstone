c38922cd0a4db958886e4dba2f835646d5523a3f
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-08-24 19:43:41 -0800

    capstone changed frontend and started backend

---
+++ public/addGame.js
@@ -0,0 +1,9 @@
+console.log("hello game")
+const form = document.getElementById("add-form")
+const gameTitle = document.getElementById("title-bar")
+const gameGenre = document.getElementById("genre-bar")
+const gameConsole = document.getElementById("console-bar")
+const gameDesc = document.getElementById("desc-bar")
+
+
+
